4c9803fd3ad632d7592fc3d5da20ac7351f6292e
ILYA.NASA <il.nasa@outlook.com>
2023-05-20 11:45:10 +0300

    eliminated unnecessary margins

--- 08_ClassesAndObjects/practice/src/main/kotlin/Main.kt
+++ 08_ClassesAndObjects/practice/src/main/kotlin/Main.kt
@@ -3,38 +3,30 @@ fun main() {
     val firstTV = TV("LG", "NANO", 49.5)
     firstTV.turnOnOffTV(true)
     firstTV.printTVInfo()
-
     repeat(6) { firstTV.uppVolumeSound(10) }
     println("Громкость + 60 = ${firstTV.volumeSound}")
     repeat(2) { firstTV.downVolumeSound(10) }
     println("Громкость - 20 = ${firstTV.volumeSound}")
     repeat(4) { firstTV.downVolumeSound(10) }
     println("Громкость - 40 = ${firstTV.volumeSound}")
-
     firstTV.printChannelInfo()
-
     firstTV.channelSwitchToButtonUppAndDown()
     firstTV.channelSwitchToButtonUppAndDown(false)
-
 //    userTV.channelSwitchToNumber(10)
 
     println()
     val secondTV = TV("Samsung", "NEO", 55.0)
     secondTV.turnOnOffTV(false)
     secondTV.printTVInfo()
-
     repeat(6) { secondTV.uppVolumeSound(10) }
     println("Громкость + 60 = ${secondTV.volumeSound}")
     repeat(2) { secondTV.downVolumeSound(10) }
     println("Громкость - 20 = ${secondTV.volumeSound}")
     repeat(4) { secondTV.downVolumeSound(10) }
     println("Громкость - 40 = ${secondTV.volumeSound}")
-
     secondTV.printChannelInfo()
-
     secondTV.channelSwitchToButtonUppAndDown()
     secondTV.channelSwitchToButtonUppAndDown()
-
     secondTV.channelSwitchToNumber(5)
 
 }
