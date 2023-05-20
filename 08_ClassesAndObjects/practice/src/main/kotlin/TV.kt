e9f62e8d0efc4b354820300907f1dd1d7acb7b57
ILYA.NASA <il.nasa@outlook.com>
2023-05-20 11:44:25 +0300

    now each TV has its own list of channels and their continuous switching

--- 08_ClassesAndObjects/practice/src/main/kotlin/TV.kt
+++ 08_ClassesAndObjects/practice/src/main/kotlin/TV.kt
@@ -1,82 +1,57 @@
-//У класса следующие свойства:
-//марка, модель, размер диагонали — эти свойства доступны для чтения
+import kotlin.random.Random
+
 class TV(private val make: String, private val model: String, private val diagonalSize: Double) {
 
-    //    включён/выключен — свойство доступно для чтения и недоступно для записи;
     var power = false
         private set
-
-    //    список каналов — внутреннее свойство телевизора, недоступно для чтения;
-    private val channelList = Channels.getRandomChannels()
-
+    private val sizeChannelList = Random.nextInt(1, 8)
+    private val channelList = Channels.getRandomChannels().subList(0, sizeChannelList)
     private var currentChanel = 0
-
     var volumeSound = MIN_VOLUME
         private set
 
-    //    Класс обладает следующими функциями:
-    /**
-     * включение/выключение;
-     */
     fun turnOnOffTV(power: Boolean) {
         this.power = power
     }
 
-    /**
-     * увеличение громкости на заданную величину;
-     */
     fun uppVolumeSound(value: Int) {
         volumeSound = minOf(MAX_VOLUME, volumeSound + value)
     }
 
-    /**
-     * уменьшение громкости на заданную величину;
-     */
     fun downVolumeSound(value: Int) {
         volumeSound = maxOf(MIN_VOLUME, volumeSound - value)
     }
 
-    /**
-     * переключение канала на заданный (аналог нажатия кнопки с цифрой канала на пульте);
-     * если телевизор выключен, то использование этой функции включает телевизор и переключает канал на заданный;
-     */
     fun channelSwitchToNumber(numChanel: Int) {
         val selectedChanel = numChanel - 1
         if (selectedChanel in channelList.indices) {
             currentChanel = selectedChanel
-            println(channelList[currentChanel])
-        }
-        else {
+            println("Канал переключен на ${channelList[currentChanel]}")
+        } else {
             println("Такого канала нет, выбери другой")
             channelSwitchToNumber(readln().toInt())
         }
     }
 
-    /**
-     * переключение каналов в порядке возрастания и убывания
-     * (аналог нажатия кнопок переключения каналов «Вверх» и «Вниз» на пульте);
-     * если телевизор выключен, то использование этих функций включает телевизор на канале, который был до выключения;
-     * переключение каналов непрерывно: после последнего канала включается первый из списка;
-     */
     fun channelSwitchToButtonUppAndDown(buttonUpp: Boolean = true) {
         if (power) {
-            if (buttonUpp) {
+            if (buttonUpp && currentChanel < sizeChannelList - 1) {
                 currentChanel++
                 println("Текущий канал ${channelList[currentChanel]}")
-            } else {
+            } else if (!buttonUpp && currentChanel > 0) {
                 currentChanel--
                 println("Текущий канал ${channelList[currentChanel]}")
+            } else {
+                currentChanel = 0
+                println("Текущий канал ${channelList[currentChanel]}")
             }
-        } //TODO непрерывное переключение каналов
+        }
         else {
             power = true
             println("Телевизор включен, текущий канал ${channelList[currentChanel]}")
         }
     }
 
-    /**
-     * представление списка каналов в виде порядковый номер - название_канала для отображения.
-     */
     fun printChannelInfo() =
         channelList.forEachIndexed { index, s -> println("Канал №${index + 1} - $s") }
 
@@ -88,8 +63,6 @@ class TV(private val make: String, private val model: String, private val diagon
         """.trimIndent()
     )
 
-    //    максимальная громкость — внутреннее свойство, значение константное,
-    //    поэтому располагается в объекте-компаньоне этого класса.
     companion object {
         const val MIN_VOLUME = 0
         const val MAX_VOLUME = 50
