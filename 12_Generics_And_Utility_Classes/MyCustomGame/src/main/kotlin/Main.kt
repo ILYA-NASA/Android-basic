08928021f02b060d2712d9548196087d1eb76578
ILYA.NASA <il.nasa@outlook.com>
2023-06-16 11:37:04 +0300

    added relevant tests

--- 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
+++ 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 fun main() {
-    val testStack = Stack("test0")
+    val testStack = Stack<String>()
+    testStack.push("test0")
     testStack.push("test1")
     testStack.push("test2")
-    testStack.push("test3")
     testStack.printAllElements()
     testStack.pop()
     testStack.printAllElements()
     testStack.pop()
     testStack.printAllElements()
+    println(testStack.isEmpty())
+    testStack.pop()
     println(testStack.isEmpty())
 }
