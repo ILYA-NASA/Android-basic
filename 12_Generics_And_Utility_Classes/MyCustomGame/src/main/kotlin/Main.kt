ee08cba14ad5ac7e41dfe0f4bebbeae5de58794a
ILYA.NASA <il.nasa@outlook.com>
2023-06-29 16:02:38 +0300

    initialised battle

--- 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
+++ 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    Team(10).printWarriors()
+    val firstTeam = Team(2)
+    val secondTeam = Team(2)
+    firstTeam.printWarriors()
+    secondTeam.printWarriors()
+    val battle = Battle(firstTeam, secondTeam)
+    repeat(3) { battle.makeBattleIteration() }
 }
