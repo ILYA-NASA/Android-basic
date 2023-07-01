4d71c4fb6e577ae0cfddca4ecbea990e2a34f001
ILYA.NASA <il.nasa@outlook.com>
2023-07-01 21:42:41 +0300

    added info and separations

--- 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
+++ 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
 fun main() {
     val firstTeam = Team(2)
     val secondTeam = Team(2)
+    println("First team warriors:")
     firstTeam.printWarriors()
+    println()
+    println("Second team warriors:")
     secondTeam.printWarriors()
+    println()
     val battle = Battle(firstTeam, secondTeam)
-    repeat(3) { battle.makeBattleIteration() }
+    for (i in 1..3) {
+        println("Iteration №${i}")
+        battle.makeBattleIteration()
+        println()
+    }
 }
