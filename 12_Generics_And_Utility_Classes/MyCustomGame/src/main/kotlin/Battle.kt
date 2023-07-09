17c748317ba97de9a3bec258a71d110accdc0de8
ILYA.NASA <il.nasa@outlook.com>
2023-07-09 15:40:08 +0300

    debugged attack and added info

--- 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Battle.kt
+++ 12_Generics_And_Utility_Classes/MyCustomGame/src/main/kotlin/Battle.kt
@@ -7,16 +7,12 @@ class Battle(
     private fun getBattleState(team: Team) = BattleState.Progress.getInfo(team)
 
     fun makeBattleIteration() {
-        var firstTeamWarrior = firstTeam.getRandomWarrior()
-        var secondTeamWarrior = secondTeam.getRandomWarrior()
-        if (firstTeamWarrior.isKilled || secondTeamWarrior.isKilled) {
-            firstTeamWarrior = firstTeam.getRandomWarrior()
-            secondTeamWarrior = secondTeam.getRandomWarrior()
-        }
-        firstTeamWarrior.makeAttack(secondTeam.getRandomWarrior())
-        println(getBattleState(firstTeam))
-        secondTeamWarrior.makeAttack(firstTeam.getRandomWarrior())
-        println(getBattleState(secondTeam))
-        gameContinues = firstTeam.isNotDestroyed() || secondTeam.isNotDestroyed()
+        val firstTeamWarrior = firstTeam.getRandomAliveWarrior()
+        val secondTeamWarrior = secondTeam.getRandomAliveWarrior()
+        firstTeamWarrior.makeAttack(secondTeamWarrior)
+        println("First attacked Second. State second team:\n${getBattleState(secondTeam)}\n")
+        secondTeamWarrior.makeAttack(firstTeamWarrior)
+        println("Second attacked First. State first team:\n${getBattleState(firstTeam)}")
+        if (firstTeam.isDestroyed() || secondTeam.isDestroyed()) gameContinues = false
     }
 }
