137c0ead77d4d22eb94d4f0104f76cd533feca01
ILYA.NASA <il.nasa@outlook.com>
2023-07-20 13:31:09 +0300

    points 3 and 4 is done

--- 14_Coroutines/FibonacciCoroutines/src/main/kotlin/Main.kt
+++ 14_Coroutines/FibonacciCoroutines/src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main() {
-    println(Fibonacci.take(10))
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch { println(Fibonacci.take(5)) }
+    launch {
+        delay(500)
+        println(Fibonacci.take(10))
+    }
+    delay(1000)
+    println(Fibonacci.take(15))
 }
