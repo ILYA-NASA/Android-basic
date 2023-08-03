437e78f1b72fc383c60106ee86c3b3959a7bbf4f
ILYA.NASA <il.nasa@outlook.com>
2023-08-03 22:58:27 +0300

    added card generator

--- 15_Flow/LottoGame/src/main/kotlin/Generator.kt
+++ 15_Flow/LottoGame/src/main/kotlin/Generator.kt
@@ -1,17 +1,21 @@
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlin.random.Random
 
 object Generator {
-    private val barrelsList = MutableList(90) { it + 1 }
+    val cardNumFlow = flow<Int> {
+        repeat(Game.CARD_ROWS_COUNT * Game.CARD_ROWS_NUM_COUNT) {
+            emit(Random.nextInt(Game.BARRELS_COUNT + 1))
+        }
+    }
+    private val barrelsList = MutableList(Game.BARRELS_COUNT) { it + 1 }
 
-    fun flow(): Flow<Int> {
+    fun barrelsFlow(): Flow<Int> {
         return flow {
             while (barrelsList.isNotEmpty()) {
                 val currentBarrel = barrelsList.random()
                 emit(currentBarrel)
                 barrelsList.remove(currentBarrel)
-                delay(300)
             }
         }
     }
