32cc60c901cf8079ab7202549bc4c9f22dd9a38c
ILYA.NASA <il.nasa@outlook.com>
2023-08-03 23:01:15 +0300

    created test game cards

--- 15_Flow/LottoGame/src/main/kotlin/Main.kt
+++ 15_Flow/LottoGame/src/main/kotlin/Main.kt
@@ -1,12 +1,39 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
         launch {
-            Generator.flow().collect {
-                println(it)
-            }
+            Generator
+                .cardNumFlow
+                .distinctUntilChanged()
+                .toList()
+                .let(::println)
+        }
+        launch {
+            Generator
+                .cardNumFlow
+                .distinctUntilChanged()
+                .toList()
+                .let(::println)
+        }
+        launch {
+            Generator
+                .cardNumFlow
+                .distinctUntilChanged()
+                .toList()
+                .let(::println)
+        }
+        launch {
+            Generator
+                .barrelsFlow()
+                .collect {
+                    delay(200)
+                    println(it)
+                }
         }
     }
 }
