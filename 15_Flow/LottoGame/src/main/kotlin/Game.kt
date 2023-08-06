93ab3eb22b42ccb991960d44b8691d89dade565a
ILYA.NASA <il.nasa@outlook.com>
2023-08-06 14:39:02 +0300

    applied DRY principle

--- 15_Flow/LottoGame/src/main/kotlin/Game.kt
+++ 15_Flow/LottoGame/src/main/kotlin/Game.kt
@@ -1,4 +1,11 @@
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+
 class Game {
+    suspend fun createCard() = NumberGenerator
+        .randomFlow()
+        .take(CARD_ROWS_COUNT * CARD_ROWS_NUM_COUNT)
+        .toList()
 
     companion object {
         const val CARD_ROWS_COUNT = 3
