8e0a2f3fe8fa6feab8006298095e203441da5340
ILYA.NASA <il.nasa@outlook.com>
2023-08-11 11:50:19 +0300

    created task buttons and click logic

--- 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
+++ 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.counterofbuspassengers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.counterofbuspassengers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.seatsStatus.text = "Все места свободны"
+        binding.seatsCounter.text = counter.toString()
+
+        binding.counterReset.setOnClickListener {
+            counter = 0
+            binding.seatsCounter.text = counter.toString()
+        }
+
+        binding.incrementButton.setOnClickListener {
+            counter++
+            binding.seatsCounter.text = counter.toString()
+            if (counter >= MAX_PASSENGERS) {
+                binding.counterReset.visibility = View.VISIBLE
+            }
+        }
+
+        binding.decrementButton.setOnClickListener {
+            maxOf(MIN_PASSENGERS, counter--).also { counter = it - 1 }
+            binding.seatsCounter.text = counter.toString()
+        }
+
+    }
+
+    companion object {
+        const val MIN_PASSENGERS = 1
+        const val MAX_PASSENGERS = 50
     }
 }
