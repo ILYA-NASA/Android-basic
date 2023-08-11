ea1e590f1442be3381c083113233e5e068fecb24
ILYA.NASA <il.nasa@outlook.com>
2023-08-11 13:00:11 +0300

    debugged and refactored app

--- 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
+++ 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
@@ -7,22 +7,31 @@ import com.example.counterofbuspassengers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.seatsStatus.text = "Все места свободны"
+        binding.emptyBus.visibility = View.VISIBLE
         binding.seatsCounter.text = counter.toString()
 
         binding.counterReset.setOnClickListener {
             counter = 0
             binding.seatsCounter.text = counter.toString()
+            binding.counterReset.visibility = View.INVISIBLE
+            binding.fullBus.visibility = View.INVISIBLE
+            binding.emptyBus.visibility = View.VISIBLE
         }
 
         binding.incrementButton.setOnClickListener {
             counter++
             binding.seatsCounter.text = counter.toString()
+            binding.emptyBus.visibility = View.INVISIBLE
+            binding.seatsStatus.visibility = View.VISIBLE
+            "Seats left: ${MAX_PASSENGERS - counter}".also { binding.seatsStatus.text = it }
             if (counter >= MAX_PASSENGERS) {
+                binding.seatsStatus.visibility = View.INVISIBLE
+                binding.fullBus.visibility = View.VISIBLE
                 binding.counterReset.visibility = View.VISIBLE
             }
         }
@@ -30,6 +39,16 @@ class MainActivity : AppCompatActivity() {
         binding.decrementButton.setOnClickListener {
             maxOf(MIN_PASSENGERS, counter--).also { counter = it - 1 }
             binding.seatsCounter.text = counter.toString()
+            if (counter < MAX_PASSENGERS) {
+                binding.fullBus.visibility = View.INVISIBLE
+                binding.counterReset.visibility = View.INVISIBLE
+                binding.seatsStatus.visibility = View.VISIBLE
+                "Seats left: ${MAX_PASSENGERS - counter}".also { binding.seatsStatus.text = it }
+            }
+            if (counter == 0) {
+                binding.seatsStatus.visibility = View.INVISIBLE
+                binding.emptyBus.visibility = View.VISIBLE
+            }
         }
 
     }
