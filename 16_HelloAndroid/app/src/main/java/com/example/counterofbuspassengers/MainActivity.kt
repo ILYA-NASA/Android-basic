1cdbb6150ac4abbe97267caec78de6721426e138
ILYA.NASA <il.nasa@outlook.com>
2023-08-13 17:40:12 +0300

    created view controller function

--- 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
+++ 16_HelloAndroid/app/src/main/java/com/example/counterofbuspassengers/MainActivity.kt
@@ -7,50 +7,60 @@ import com.example.counterofbuspassengers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var counter = 0
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.freeSeatsTextView.visibility = View.VISIBLE
-        binding.counterTextView.text = counter.toString()
-
         binding.resetButton.setOnClickListener {
             counter = 0
-            binding.counterTextView.text = counter.toString()
-            binding.resetButton.visibility = View.INVISIBLE
-            binding.tooManyPassengersTextView.visibility = View.INVISIBLE
-            binding.freeSeatsTextView.visibility = View.VISIBLE
+            setVisibleView(0)
         }
-
         binding.plusButton.setOnClickListener {
             counter++
-            binding.counterTextView.text = counter.toString()
-            binding.freeSeatsTextView.visibility = View.INVISIBLE
-            binding.currentSeatsTextView.visibility = View.VISIBLE
-            "Seats left: ${MAX_PASSENGERS - counter}".also { binding.currentSeatsTextView.text = it }
-            if (counter >= MAX_PASSENGERS) {
-                binding.currentSeatsTextView.visibility = View.INVISIBLE
-                binding.tooManyPassengersTextView.visibility = View.VISIBLE
-                binding.resetButton.visibility = View.VISIBLE
-            }
+            setVisibleView(1)
         }
-
         binding.minusButton.setOnClickListener {
             maxOf(MIN_PASSENGERS, counter--).also { counter = it - 1 }
-            binding.counterTextView.text = counter.toString()
-            if (counter < MAX_PASSENGERS) {
-                binding.tooManyPassengersTextView.visibility = View.INVISIBLE
+            setVisibleView(2)
+        }
+    }
+
+    private fun setVisibleView(currentView: Int) {
+        binding.counterTextView.text = counter.toString()
+        "Seats left: ${MAX_PASSENGERS - counter}".also {
+            binding.currentSeatsTextView.text = it
+        }
+        when (currentView) {
+            0 -> {
                 binding.resetButton.visibility = View.INVISIBLE
-                binding.currentSeatsTextView.visibility = View.VISIBLE
-                "Seats left: ${MAX_PASSENGERS - counter}".also { binding.currentSeatsTextView.text = it }
-            }
-            if (counter == 0) {
-                binding.currentSeatsTextView.visibility = View.INVISIBLE
+                binding.tooManyPassengersTextView.visibility = View.INVISIBLE
                 binding.freeSeatsTextView.visibility = View.VISIBLE
             }
-        }
 
+            1 -> {
+                binding.freeSeatsTextView.visibility = View.INVISIBLE
+                binding.currentSeatsTextView.visibility = View.VISIBLE
+                if (counter >= MAX_PASSENGERS) {
+                    binding.currentSeatsTextView.visibility = View.INVISIBLE
+                    binding.tooManyPassengersTextView.visibility = View.VISIBLE
+                    binding.resetButton.visibility = View.VISIBLE
+                }
+            }
+
+            2 -> {
+                if (counter < MAX_PASSENGERS) {
+                    binding.tooManyPassengersTextView.visibility = View.INVISIBLE
+                    binding.resetButton.visibility = View.INVISIBLE
+                    binding.currentSeatsTextView.visibility = View.VISIBLE
+                }
+                if (counter == 0) {
+                    binding.currentSeatsTextView.visibility = View.INVISIBLE
+                    binding.freeSeatsTextView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     companion object {
