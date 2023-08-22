8c864fda8c6b46ed0e77ae11624e09eaa0413f69
ILYA.NASA <il.nasa@outlook.com>
2023-08-22 17:03:57 +0300

    added slider

--- 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ProgressBar
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     private var currentProgress = 0
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         val circleProgress = findViewById<ProgressBar>(R.id.progressBarCircular)
         val buttonStartStop = findViewById<Button>(R.id.buttonStartStop)
         buttonStartStop.text = "START"
+        val slider = findViewById<Slider>(R.id.slider)
 
         val updateProgress = {
             circleProgress.progress = currentProgress
