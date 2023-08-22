237a081ee2994f8e7806bb6e5e6242c572e87521
ILYA.NASA <il.nasa@outlook.com>
2023-08-22 15:33:42 +0300

    linked button to progress bar

--- 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.countdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+    private var currentProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val circleProgress = findViewById<ProgressBar>(R.id.progressBarCircular)
+        val buttonStartStop = findViewById<Button>(R.id.buttonStartStop)
+        buttonStartStop.text = "START"
+
+        val updateProgress = {
+            circleProgress.progress = currentProgress
+        }
+
+        buttonStartStop.setOnClickListener {
+            currentProgress++
+            buttonStartStop.text = "STOP"
+            updateProgress()
+        }
     }
 }
