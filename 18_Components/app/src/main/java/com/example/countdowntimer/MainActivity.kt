5d98167335a1a9389ab9e56dc8d1e7cf55a5edeb
ILYA.NASA <il.nasa@outlook.com>
2023-08-25 14:10:13 +0300

    added viewBinding method

--- 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -1,30 +1,22 @@
 package com.example.countdowntimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ProgressBar
-import com.google.android.material.slider.Slider
+import androidx.appcompat.app.AppCompatActivity
+import com.example.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var currentProgress = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonStartStop.text = "START"
 
-        val circleProgress = findViewById<ProgressBar>(R.id.progressBarCircular)
-        val buttonStartStop = findViewById<Button>(R.id.buttonStartStop)
-        buttonStartStop.text = "START"
-        val slider = findViewById<Slider>(R.id.slider)
-
-        val updateProgress = {
-            circleProgress.progress = currentProgress
-        }
-
-        buttonStartStop.setOnClickListener {
+        binding.buttonStartStop.setOnClickListener {
             currentProgress++
-            buttonStartStop.text = "STOP"
-            updateProgress()
+            binding.buttonStartStop.text = "STOP"
+            binding.progressBarCircular.progress = currentProgress
         }
     }
 }
