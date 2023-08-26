b03f8d1bdace69b6d0f79ca26d5d871bcdd65b50
ILYA.NASA <il.nasa@outlook.com>
2023-08-26 12:37:36 +0300

    refactored code and fixed bug to mutable click

--- 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ 18_Components/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -18,43 +17,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val updateProgress = {
-            binding.slider.isEnabled = true
-            binding.buttonStart.visibility = View.VISIBLE
-            binding.textTimerTaskFinished.visibility = View.INVISIBLE
-            binding.buttonStop.visibility = View.INVISIBLE
-            currentProgress = binding.slider.value.toInt()
-            binding.progressBarCircular.max = currentProgress
-            binding.textCounter.text = currentProgress.toString()
-            binding.progressBarCircular.progress = currentProgress
-        }
         updateProgress()
         binding.slider.addOnChangeListener { _, _, _ ->
             updateProgress()
         }
         binding.buttonStart.setOnClickListener {
-            binding.slider.isEnabled = false
-            binding.buttonStart.visibility = View.INVISIBLE
-            binding.buttonStop.visibility = View.VISIBLE
-            val scope = CoroutineScope(Dispatchers.Main)
-            scope.launch {
-                while (scope.isActive) {
-                    delay(1000)
-                    currentProgress--
-                    binding.textCounter.text = currentProgress.toString()
-                    binding.progressBarCircular.progress = currentProgress
-                    if (currentProgress == 0) {
-                        binding.textTimerTaskFinished.visibility = View.VISIBLE
-                        delay(1000)
-                        scope.cancel()
-                    }
-                    binding.buttonStop.setOnClickListener {
-                        scope.cancel()
-                        updateProgress()
-                    }
+            startCountdownTimer()
+        }
+    }
+
+    private fun updateProgress() {
+        binding.slider.isEnabled = true
+        binding.buttonStart.visibility = View.VISIBLE
+        binding.textTimerTaskFinished.visibility = View.INVISIBLE
+        binding.buttonStop.visibility = View.INVISIBLE
+        currentProgress = binding.slider.value.toInt()
+        binding.progressBarCircular.max = currentProgress
+        binding.textCounter.text = currentProgress.toString()
+        binding.progressBarCircular.progress = currentProgress
+    }
+
+    private fun startCountdownTimer() {
+        binding.slider.isEnabled = false
+        binding.buttonStart.visibility = View.INVISIBLE
+        binding.buttonStop.visibility = View.VISIBLE
+        val scope = CoroutineScope(Dispatchers.Main)
+        scope.launch {
+            while (currentProgress != 0) {
+                binding.buttonStop.setOnClickListener {
+                    scope.cancel()
+                    updateProgress()
                 }
-                updateProgress.invoke()
+                delay(1000)
+                currentProgress--
+                binding.textCounter.text = currentProgress.toString()
+                binding.progressBarCircular.progress = currentProgress
             }
+            binding.textTimerTaskFinished.visibility = View.VISIBLE
+            delay(1000)
+            scope.cancel()
+            updateProgress()
         }
     }
 }
