e2351ab7860ba5e87c5d2cd06344f656e18ac37a
ILYA.NASA <il.nasa@outlook.com>
2023-09-17 18:00:18 +0300

    added viewBinding method

--- 20_Quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
+++ 20_Quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.skillbox_hw_quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.skillbox_hw_quiz.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.example.skillbox_hw_quiz.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
